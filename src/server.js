cc3f18ec3ae36f76403101474b476df385effb4a
Antonio <abamorim36@gmail.com>
2023-10-20 08:33:45 -0300

    Structuring Routes - List/Create

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import http from 'node:http'
 
+const tasks = []
+
 const server = http.createServer((req, res) => {
+     const method = req.method
+
+     const url = req.url
+
+     console.log(method, url)
+
      return res.end('Oi World')
 })
 
